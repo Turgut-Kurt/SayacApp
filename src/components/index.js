d727fe0801cf24a0adc630604767193dcf9b3cac
Turgut Kurt <305433@ogr.ktu.edu.tr>
2021-08-19 16:49:02 +0300

    update

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,6 @@
 export * from './config';
 export * from './CustomButtonWithSvg';
-<<<<<<< HEAD
 export * from './SearchInput';
 export * from './HouseCard';
 export * from './StatusBadge';
 export * from './StatusHeader';
-=======
-export * from './CustomButton';
-export * from './CustomInputLabel';
->>>>>>> main
--- src/screens/HousesScreen/index.js
+++ src/screens/HousesScreen/index.js
@@ -1,95 +1,45 @@
-<<<<<<< HEAD
-import {FlatList, Text, View,} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
+import {HouseCard, SearchInput, StatusBadge} from '~components';
 
-import { CustomButtonWithSvg } from '~components';
-import {SearchInput ,HouseCard,StatusBadge} from '~components';
+import {CustomButtonWithSvg} from '~components';
 import React from 'react';
+import {colors} from '../../components/config/colors';
 import {home_filter} from '~assets';
-import { sizes } from '../../components/config/fonts';
-import {colors} from '../../components/config/colors'
+import {sizes} from '../../components/config/fonts';
 
 const data = {
-  cards: [{
-    name: "Mehmet ÖZKAN",
-    an: 1234567,
-    meter: 123556,
-    address: "Aşağı Mah. Ata Cd. Kavuncu Sk. No: 12",
-  },
+  cards: [
     {
-    name: "Fadime DURAN",
-    an: 7654321,
-    meter: 587532,
-    address: "Aşağı Mah. Ata Cd. Kavuncu Sk.",
-  }]
-}
-
+      name: 'Mehmet ÖZKAN',
+      an: 1234567,
+      meter: 123556,
+      address: 'Aşağı Mah. Ata Cd. Kavuncu Sk. No: 12',
+    },
+    {
+      name: 'Fadime DURAN',
+      an: 7654321,
+      meter: 587532,
+      address: 'Aşağı Mah. Ata Cd. Kavuncu Sk.',
+    },
+  ],
+};
 
 const HousesScreen = () => {
-
-  const onSearch = (val) => {
-  console.log(val)
-}
+  const onSearch = val => {
+    console.log(val);
+  };
 
   return (
     <View style={{backgroundColor: '#ffffff'}}>
-      <View style={{marginHorizontal:16}}>
-        <SearchInput
-        onChange={(val) => onSearch(val)}
-        />
-    </View>
-      
-      
+      <View style={{marginHorizontal: 16}}>
+        <SearchInput onChange={val => onSearch(val)} />
+      </View>
+
       <FlatList
         renderItem={({item}) => <HouseCard {...item} />}
         data={data.cards}
         keyExtractor={(item, index) => index.toString()}
       />
-           
-      
-=======
-import {
-  CustomButton,
-  CustomButtonWithSvg,
-  CustomInputLabel,
-  colors,
-} from '~components';
-import React, {useState} from 'react';
-import {Text, View} from 'react-native';
-import {Formik} from 'formik';
-import {LoginValidationSchema} from '~schema';
-import {home_filter} from '~assets';
-const HousesScreen = () => {
-  const [formikInitialValues, setFormikinitialValues] = useState({
-    email: '',
-  });
-  return (
-    <View style={{backgroundColor: '#ffffff'}}>
-      <Text>HousesScreen</Text>
-      <CustomButtonWithSvg />
-      <CustomButtonWithSvg />
-      <CustomButtonWithSvg svg={home_filter} text={'Filtrele'} />
-      <CustomButton />
-      <Formik
-        validationSchema={LoginValidationSchema}
-        initialValues={formikInitialValues}
-        onSubmit={values => {
-          console.log(values);
-        }}>
-        {() => (
-          <View style={{backgroundColor: colors.MainWhite, paddingTop: 30}}>
-            <CustomInputLabel
-              name={'email'}
-              placeholder={'turgutkurt@gmail.com'}
-              label={'E-posta adresi'}
-              containerProps={{
-                keyboardType: 'email-address',
-                maxLength: 40,
-              }}
-            />
-          </View>
-        )}
-      </Formik>
->>>>>>> main
     </View>
   );
 };
