9fde3260462d8fc672f85177c744a85b4f30aa6c
MaegIiN <ramis.seker@gmail.com>
2021-08-19 15:32:41 +0300

    House Card-Search Input-StatusBadge-StatusHeader Components Added

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,6 @@
 export * from './config';
 export * from './CustomButtonWithSvg';
+export * from './SearchInput';
+export * from './HouseCard';
+export * from './StatusBadge';
+export * from './StatusHeader';
--- src/screens/HousesScreen/index.js
+++ src/screens/HousesScreen/index.js
@@ -1,17 +1,50 @@
-import {Text, View} from 'react-native';
+import {FlatList, Text, View,} from 'react-native';
 
-import {CustomButtonWithSvg} from '~components';
+import { CustomButtonWithSvg } from '~components';
+import {SearchInput ,HouseCard,StatusBadge} from '~components';
 import React from 'react';
 import {home_filter} from '~assets';
-import {sizes} from '../../components/config/fonts';
+import { sizes } from '../../components/config/fonts';
+import {colors} from '../../components/config/colors'
+
+const data = {
+  cards: [{
+    name: "Mehmet ÖZKAN",
+    an: 1234567,
+    meter: 123556,
+    address: "Aşağı Mah. Ata Cd. Kavuncu Sk. No: 12",
+  },
+    {
+    name: "Fadime DURAN",
+    an: 7654321,
+    meter: 587532,
+    address: "Aşağı Mah. Ata Cd. Kavuncu Sk.",
+  }]
+}
+
 
 const HousesScreen = () => {
+
+  const onSearch = (val) => {
+  console.log(val)
+}
+
   return (
     <View style={{backgroundColor: '#ffffff'}}>
-      <Text style={{fontSize: sizes.t20}}>HousesScreen</Text>
-      <CustomButtonWithSvg />
-      <CustomButtonWithSvg />
-      <CustomButtonWithSvg svg={home_filter} text={'Filtrele'} />
+      <View style={{marginHorizontal:16}}>
+        <SearchInput
+        onChange={(val) => onSearch(val)}
+        />
+    </View>
+      
+      
+      <FlatList
+        renderItem={({item}) => <HouseCard {...item} />}
+        data={data.cards}
+        keyExtractor={(item, index) => index.toString()}
+      />
+           
+      
     </View>
   );
 };
