7451879b0fe25e86301ba9b64b630ccf19b1f73a
Turgut Kurt <305433@ogr.ktu.edu.tr>
2021-09-05 11:08:32 +0300

    AddHouseScreen added keyboard avoid

--- src/screens/HousesScreen/AddHouseScreen/index.js
+++ src/screens/HousesScreen/AddHouseScreen/index.js
@@ -13,6 +13,7 @@ import {fontSize, goBack} from '~utils';
 
 import {AddHouseValidationSchema} from '~schema';
 import {Formik} from 'formik';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import SQLite from 'react-native-sqlite-storage';
 import VectorImage from 'react-native-vector-image';
 import {db} from '~request';
@@ -82,163 +83,172 @@ const AddHouseScreen = () => {
     });
   };
   return (
-    <Formik
-      validationSchema={AddHouseValidationSchema}
-      initialValues={formikInitialValues}
-      //onSubmit={values => console.log(values)}
-    >
-      {({handleChange, handleBlur, handleSubmit, values, errors, isValid}) => (
-        <View style={styles.Container}>
-          <CustomCommonHeader
-            data={data}
-            activeBottom={false}
-            backButton={
-              <TouchableOpacity
-                onPress={() => goBack()}
-                style={styles.CustomBack}>
-                <VectorImage source={arrow} />
-                <Text style={styles.CustomBackText}>Hane Ekle</Text>
-              </TouchableOpacity>
-            }
-            svg={home_logo}
-          />
-          <CustomInputLabel
-            name={'name'}
-            containerProps={{
-              label: 'Ad soyad',
-              placeholder: '',
-              maxLength: 25,
-            }}
-            succesColor={colors.MainGreen}
-            errorColor={colors.MainRed}
-          />
-          <CustomInputLabel
-            name={'tcno'}
-            containerProps={{
-              keyboardType: 'numeric',
-              label: 'TC kimlik no',
-              placeholder: '',
-              maxLength: 11,
-            }}
-            succesColor={colors.MainGreen}
-            errorColor={colors.MainRed}
-            phone={true}
-          />
-          <View style={styles.BinaryInput}>
+    <KeyboardAwareScrollView style={{flex: 1}}>
+      <Formik
+        validationSchema={AddHouseValidationSchema}
+        initialValues={formikInitialValues}
+        //onSubmit={values => console.log(values)}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          isValid,
+        }) => (
+          <View style={styles.Container}>
+            <CustomCommonHeader
+              data={data}
+              activeBottom={false}
+              backButton={
+                <TouchableOpacity
+                  onPress={() => goBack()}
+                  style={styles.CustomBack}>
+                  <VectorImage source={arrow} />
+                  <Text style={styles.CustomBackText}>Hane Ekle</Text>
+                </TouchableOpacity>
+              }
+              svg={home_logo}
+            />
             <CustomInputLabel
-              name={'neighbourhood'}
+              name={'name'}
               containerProps={{
-                label: 'Mahalle',
+                label: 'Ad soyad',
                 placeholder: '',
-                maxLength: 15,
+                maxLength: 25,
               }}
               succesColor={colors.MainGreen}
               errorColor={colors.MainRed}
-              inputContainerStyle={styles.inputContainer}
-              containerStyle={styles.inputContainer}
-              labelStyle={styles.labelAndError}
-              errorStyle={styles.labelAndError}
             />
             <CustomInputLabel
-              name={'street'}
-              containerProps={{
-                label: 'Cadde',
-                placeholder: '',
-                maxLength: 15,
-              }}
-              succesColor={colors.MainGreen}
-              errorColor={colors.MainRed}
-              inputContainerStyle={styles.inputContainer}
-              containerStyle={styles.inputContainer}
-              labelStyle={styles.labelAndError}
-              errorStyle={styles.labelAndError}
-            />
-          </View>
-          <View style={styles.BinaryInput}>
-            <CustomInputLabel
-              name={'doornumber'}
-              containerProps={{
-                label: 'Sokak/kapı no',
-                placeholder: '',
-                maxLength: 15,
-              }}
-              succesColor={colors.MainGreen}
-              errorColor={colors.MainRed}
-              inputContainerStyle={styles.inputContainer}
-              containerStyle={styles.inputContainer}
-              labelStyle={styles.labelAndError}
-              errorStyle={styles.labelAndError}
-            />
-            <CustomInputLabel
-              name={'counternumber'}
+              name={'tcno'}
               containerProps={{
                 keyboardType: 'numeric',
-                label: 'Sayaç no',
+                label: 'TC kimlik no',
                 placeholder: '',
-                maxLength: 10,
+                maxLength: 11,
               }}
               succesColor={colors.MainGreen}
               errorColor={colors.MainRed}
-              inputContainerStyle={styles.inputContainer}
-              containerStyle={styles.inputContainer}
-              labelStyle={styles.labelAndError}
-              errorStyle={styles.labelAndError}
               phone={true}
             />
-          </View>
-          <View style={styles.BinaryInput}>
+            <View style={styles.BinaryInput}>
+              <CustomInputLabel
+                name={'neighbourhood'}
+                containerProps={{
+                  label: 'Mahalle',
+                  placeholder: '',
+                  maxLength: 15,
+                }}
+                succesColor={colors.MainGreen}
+                errorColor={colors.MainRed}
+                inputContainerStyle={styles.inputContainer}
+                containerStyle={styles.inputContainer}
+                labelStyle={styles.labelAndError}
+                errorStyle={styles.labelAndError}
+              />
+              <CustomInputLabel
+                name={'street'}
+                containerProps={{
+                  label: 'Cadde',
+                  placeholder: '',
+                  maxLength: 15,
+                }}
+                succesColor={colors.MainGreen}
+                errorColor={colors.MainRed}
+                inputContainerStyle={styles.inputContainer}
+                containerStyle={styles.inputContainer}
+                labelStyle={styles.labelAndError}
+                errorStyle={styles.labelAndError}
+              />
+            </View>
+            <View style={styles.BinaryInput}>
+              <CustomInputLabel
+                name={'doornumber'}
+                containerProps={{
+                  label: 'Sokak/kapı no',
+                  placeholder: '',
+                  maxLength: 15,
+                }}
+                succesColor={colors.MainGreen}
+                errorColor={colors.MainRed}
+                inputContainerStyle={styles.inputContainer}
+                containerStyle={styles.inputContainer}
+                labelStyle={styles.labelAndError}
+                errorStyle={styles.labelAndError}
+              />
+              <CustomInputLabel
+                name={'counternumber'}
+                containerProps={{
+                  keyboardType: 'numeric',
+                  label: 'Sayaç no',
+                  placeholder: '',
+                  maxLength: 10,
+                }}
+                succesColor={colors.MainGreen}
+                errorColor={colors.MainRed}
+                inputContainerStyle={styles.inputContainer}
+                containerStyle={styles.inputContainer}
+                labelStyle={styles.labelAndError}
+                errorStyle={styles.labelAndError}
+                phone={true}
+              />
+            </View>
+            <View style={styles.BinaryInput}>
+              <CustomInputLabel
+                name={'initialcountervalue'}
+                containerProps={{
+                  keyboardType: 'numeric',
+                  label: 'İlk sayaç değeri',
+                  placeholder: '',
+                  maxLength: 10,
+                }}
+                succesColor={colors.MainGreen}
+                errorColor={colors.MainRed}
+                inputContainerStyle={styles.inputContainer}
+                containerStyle={styles.inputContainer}
+                labelStyle={styles.labelAndError}
+                errorStyle={styles.labelAndError}
+                phone={true}
+              />
+              <CustomInputLabel
+                name={'subscriberno'}
+                containerProps={{
+                  keyboardType: 'numeric',
+                  label: 'Abone no',
+                  placeholder: '',
+                  maxLength: 10,
+                }}
+                succesColor={colors.MainGreen}
+                errorColor={colors.MainRed}
+                inputContainerStyle={styles.inputContainer}
+                containerStyle={styles.inputContainer}
+                labelStyle={styles.labelAndError}
+                errorStyle={styles.labelAndError}
+                phone={true}
+              />
+            </View>
             <CustomInputLabel
-              name={'initialcountervalue'}
+              name={'notes'}
               containerProps={{
                 keyboardType: 'numeric',
-                label: 'İlk sayaç değeri',
+                label: 'Notlar',
                 placeholder: '',
-                maxLength: 10,
+                maxLength: 25,
               }}
               succesColor={colors.MainGreen}
               errorColor={colors.MainRed}
-              inputContainerStyle={styles.inputContainer}
-              containerStyle={styles.inputContainer}
-              labelStyle={styles.labelAndError}
-              errorStyle={styles.labelAndError}
-              phone={true}
             />
-            <CustomInputLabel
-              name={'subscriberno'}
-              containerProps={{
-                keyboardType: 'numeric',
-                label: 'Abone no',
-                placeholder: '',
-                maxLength: 10,
-              }}
-              succesColor={colors.MainGreen}
-              errorColor={colors.MainRed}
-              inputContainerStyle={styles.inputContainer}
-              containerStyle={styles.inputContainer}
-              labelStyle={styles.labelAndError}
-              errorStyle={styles.labelAndError}
-              phone={true}
+            <CustomButton
+              textName={'Kaydet'}
+              onPress={() => createData(values)}
+              buttonStyle={styles.Button}
             />
           </View>
-          <CustomInputLabel
-            name={'notes'}
-            containerProps={{
-              keyboardType: 'numeric',
-              label: 'Notlar',
-              placeholder: '',
-              maxLength: 25,
-            }}
-            succesColor={colors.MainGreen}
-            errorColor={colors.MainRed}
-          />
-          <CustomButton
-            textName={'Kaydet'}
-            onPress={() => createData(values)}
-            buttonStyle={styles.Button}
-          />
-        </View>
-      )}
-    </Formik>
+        )}
+      </Formik>
+    </KeyboardAwareScrollView>
   );
 };
 export {AddHouseScreen};
