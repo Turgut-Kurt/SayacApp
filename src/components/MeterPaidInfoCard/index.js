0889e68687eafba8c3294f84e7c4c8f23c5ff27c
Turgut Kurt <305433@ogr.ktu.edu.tr>
2021-09-11 17:03:20 +0300

    mini update

--- src/components/MeterPaidInfoCard/index.js
+++ src/components/MeterPaidInfoCard/index.js
@@ -1,31 +1,33 @@
+import { Button, Image, Text, TextInput, View } from 'react-native';
+import { PropTypes, ViewPropTypes, colors } from '~/components/config';
 import React,{useState} from 'react';
-import { View, Text, Image, Button, TextInput } from 'react-native';
+
 import VectorImage from 'react-native-vector-image';
-import { colors, PropTypes, ViewPropTypes } from '~/components/config';
-import styles from './styles';
 import { checkGray } from '~assets';
+import styles from './styles';
 
 const MeterPaidInfoCard = (props) => {
     const { onPress, meterReadTime, meterValue, delayAmount, amount} = props;
    
    
     return (
-        <View style={styles.container}>
-             <View style={styles.bottom}>
-                <VectorImage style={styles.svg} source={checkGray} />
-                <Text style={styles.info}>Ödeme Alındı</Text>
-                <Text style={styles.meterReadTime}>{meterReadTime}</Text>
-            </View>
-            
-            <View style={styles.lines}>
-                <Text style={styles.info}>Alınan:</Text>
-                <Text style={styles.values}>{amount}</Text>
-            </View>
-            <View style={styles.lines}>
-                <Text style={styles.info, {color:colors.MainBlue}}>Tamamlandı</Text>
-                
-            </View>
+      <View style={styles.container}>
+        <View style={styles.bottom}>
+          <VectorImage style={styles.svg} source={checkGray} />
+          <Text style={styles.info}>Ödeme Alındı</Text>
+          <Text style={styles.meterReadTime}>{meterReadTime}</Text>
         </View>
+
+        <View style={styles.lines}>
+          <Text style={styles.info}>Alınan:</Text>
+          <Text style={styles.values}>{amount}</Text>
+        </View>
+        <View style={styles.lines}>
+          <Text style={(styles.info, {color: colors.MainBlue})}>
+            Tamamlandı
+          </Text>
+        </View>
+      </View>
     );
 };
 
